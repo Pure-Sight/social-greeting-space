f9263c5863b1b9a2f8fffad85365fd6a635054b6
dobrzynskikam <dobrzynskikam@gmail.com>
2025-05-23 15:36:07 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,7 +14,7 @@ const Index = () => {
           <div className="flex flex-col md:flex-row items-center justify-between gap-8">
             <div className="md:w-1/2">
               <h1 className="text-4xl md:text-5xl font-bold mb-6 text-gray-900">
-                OptyClear
+                Pure Sight
               </h1>
               <p className="text-xl md:text-2xl font-medium mb-6 text-blue-600">
                 Profesjonalna stacja czyszcząca do okularów
